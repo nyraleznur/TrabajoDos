4caf8d65935783b45ff3f12915652205e0f66923
Nurdcs <nurderly@gmail.com>
2022-08-13 12:58:09 -0500

    nuevo

---
+++ OneDrive/Documents/NetBeansProjects/basedatoscero/src/test/java/com/example/basedatoscero/BasedatosceroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basedatoscero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasedatosceroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
